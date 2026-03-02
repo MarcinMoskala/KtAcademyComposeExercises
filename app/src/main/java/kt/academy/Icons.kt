66e3d3f3a88596588390aee1a6d18a5d6ef87fcb
Marcin Moskała <marcinmoskala@gmail.com>
2026-03-02 13:18:42 +0100

    Update

--- app/src/main/java/kt/academy/Icons.kt
+++ app/src/main/java/kt/academy/Icons.kt
@@ -679,3 +679,37 @@ val ComposeLogoIcon: ImageVector by lazy {
         }
     }.build()
 }
+
+val AddIcon: ImageVector by lazy {
+    ImageVector.Builder(
+        name = "Add",
+        defaultWidth = 24.0.dp,
+        defaultHeight = 24.0.dp,
+        viewportWidth = 24.0f,
+        viewportHeight = 24.0f
+    ).path(
+        fill = SolidColor(Color.Black),
+        stroke = null,
+        strokeLineWidth = 0.0f,
+        strokeLineCap = Butt,
+        strokeLineJoin = Miter,
+        strokeLineMiter = 4.0f,
+        pathFillType = NonZero
+    ) {
+        moveTo(19.0f, 13.0f)
+        horizontalLineTo(13.0f)
+        verticalLineTo(19.0f)
+        horizontalLineTo(11.0f)
+        verticalLineTo(13.0f)
+        horizontalLineTo(5.0f)
+        verticalLineTo(11.0f)
+        horizontalLineTo(11.0f)
+        verticalLineTo(5.0f)
+        horizontalLineTo(13.0f)
+        verticalLineTo(11.0f)
+        horizontalLineTo(19.0f)
+        verticalLineTo(13.0f)
+        close()
+    }.build()
+}
+
