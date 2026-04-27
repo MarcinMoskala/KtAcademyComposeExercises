1e9475674acb95f1f057a11f79a0ad948736d95c
Marcin Moskała <marcinmoskala@gmail.com>
2026-04-27 04:51:50 +0200

    Corrections

--- app/src/main/java/kt/academy/examples/GameScreen.kt
+++ app/src/main/java/kt/academy/examples/GameScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -88,5 +89,6 @@ fun SelectAnswer(blocks: List<String>, onSelect: (String) -> Unit) {
 @Preview(showBackground = true)
 @Composable
 fun GameScreenPreview() {
-    GameScreen(vm = GameViewModel())
+    val vm = remember { GameViewModel() }
+    GameScreen(vm = vm)
 }
